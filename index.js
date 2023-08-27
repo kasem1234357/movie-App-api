39857bfaa7b8c02fd5db903c042b09874ac23400
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2023-08-27 18:16:42 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,15 +6,37 @@ const cors = require( 'cors' );
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 dotenv.config();
+const corsOptions = {
+  origin: 'https://midnightx.vercel.app/', // Specify the allowed origin(s) here
+  methods: 'GET,HEAD,OPTIONS,POST,PUT,DELETE',
+  allowedHeaders: 'Origin, Content-Type, X-Requested-With, Accept, Authorization',
+};
+
+app.use(cors(corsOptions));
+// app.use(function(req,res,next){
+//    res.header('Access-Control-Allow-Origin',"*")
+//    res.header(
+//     'Access-Control-Allow-Methods',
+//     "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+//    );
+//    res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, Content-Type, X-Requested-With, Accept, Authorization'
+//    );
+//    next();
+// })
 mongoose.set('strictQuery', false);
 mongoose.connect(
   process.env.MONGO_URL,
   { useNewUrlParser: true },
-  () => {
-    console.log("Connected to MongoDB");
+  (error) => {
+    if (error) {
+      console.error('Failed to connect to MongoDB:', error);
+    } else {
+      console.log('Connected to MongoDB');
+    }
   }
 );
-app.use( cors() );
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
