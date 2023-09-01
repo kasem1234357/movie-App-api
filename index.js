07b43881877735a3642e541b6624cacf5cfa0b1b
kasem1234357 <73896080+kasem1234357@users.noreply.github.com>
2023-09-01 07:25:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,33 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const helmet = require('helmet');
 const cors = require( 'cors' );
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 dotenv.config();
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 const corsOptions = {
   origin: '*', // Specify the allowed origin(s) here
   methods: 'GET,HEAD,OPTIONS,POST,PUT,DELETE',
   allowedHeaders: 'Origin, Content-Type, X-Requested-With, Accept, Authorization',
 };
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 app.use(cors(corsOptions));
-app.use(function(req,res,next){
-   res.header('Access-Control-Allow-Origin',"*")
-   res.header(
-    'Access-Control-Allow-Methods',
-    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-   );
-   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, Content-Type, X-Requested-With, Accept, Authorization'
-   );
-   next();
-})
+// app.use(function(req,res,next){
+//    res.header('Access-Control-Allow-Origin',"*")
+//    res.header(
+//     'Access-Control-Allow-Methods',
+//     "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+//    );
+//    res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, Content-Type, X-Requested-With, Accept, Authorization'
+//    );
+//    next();
+// })
 mongoose.set('strictQuery', false);
 mongoose.connect(
   process.env.MONGO_URL,
@@ -37,7 +41,6 @@ mongoose.connect(
     }
   }
 );
-app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.listen(8800, () => {
