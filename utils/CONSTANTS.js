7cee60b71b0e5788f05364b6d5a59cc79c27e9aa
kasem1234357 <kasemalolo2002@gmail.com>
2024-01-03 18:00:25 +0200

    verification implement

--- utils/CONSTANTS.js
+++ utils/CONSTANTS.js
@@ -1,12 +1,12 @@
-export const REPORT_STATES = {
+ const REPORT_STATES = {
     NOT_READ: 1,
     IN_PROGRESS: 2,
     FIXED: 3,
   };
-export const GET_VERIFIED_EMAIL_MASSEGE = (code)=>{
+const GET_VERIFIED_EMAIL_MASSEGE = (code)=>{
    return `the code for verfication is \n ${code}`
 }
-export const SET_VERIFIED_EMAIL_MASSEGE =()=>{
+ const SET_VERIFIED_EMAIL_MASSEGE =()=>{
   return `you are verified now `
 }
 module.exports = {
